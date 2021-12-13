b01c5740f0819340f55615e098cc0448e6df32d7
Gustaf <twirrz@gmail.com>
2021-12-13 08:49:14 +0100

    cleanup day12

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -5,19 +5,20 @@ use lazy_static::lazy_static;
 use regex::Regex;
 use std::collections::HashSet;
 use std::time::Instant;
-
-
+use std::collections::HashMap;
 
 fn main() -> io::Result<()> {
-    
     let file = File::open("input_day12")?;
-    let ways: Vec<_> = BufReader::new(file).lines().map(|line|{
+    let mut ways: HashMap<String,Vec<String>> = HashMap::new();
+    BufReader::new(file).lines().for_each(|line|{
         let string = line.unwrap();
         let mut split = string.split("-");
-        let to = split.next().unwrap().to_string();
-        let from = split.next().unwrap().to_string();
-        (to,from)
-    }).collect();
+        let to = split.next().unwrap();
+        let from = split.next().unwrap();
+
+        ways.entry(String::from(to)).or_insert(<Vec<String>>::new()).push(String::from(from));
+        ways.entry(String::from(from)).or_insert(<Vec<String>>::new()).push(String::from(to));
+    });
 
     println!("Result1 {}",get_paths(true,ways.clone()));
 
@@ -30,54 +31,45 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn get_paths(visit_once: bool, ways: Vec<(String,String)>) -> u32{
+fn get_paths(visit_once: bool, ways: HashMap<String,Vec<String>>) -> u32{
     let mut paths = Vec::new();
     let mut initial_path = Vec::new();
     initial_path.push(String::from("start"));
     paths.push((initial_path,visit_once));
     let mut finished_paths = 0;
-    while paths.len() > 0{
-        let mut new_paths = Vec::new();
-        for path in paths.clone(){
-            find_continued(ways.clone(), path, &mut new_paths, &mut finished_paths);
-        }
-        paths = new_paths;
+    let mut visited = HashSet::new();
+    visited.insert(String::from("start"));
+
+    for path in paths.clone(){
+        find_continued(ways.clone(), path, &mut finished_paths, visited.clone());
     }
+
     finished_paths
 }
 
-fn find_continued(ways: Vec<(String,String)>,path: (Vec<String>,bool),new_paths: &mut Vec<(Vec<String>,bool)>, finished_paths: &mut u32){
+fn find_continued(ways: HashMap<String,Vec<String>>, path: (Vec<String>,bool), finished_paths: &mut u32, actual_visited: HashSet<String>){
     let current = path.0.last().unwrap();
-    let actual_visited: HashSet<_> = path.0.clone().into_iter().filter(|node| small_cave(node)).collect();
     let visited;
     if path.1 {
         visited = actual_visited.clone();
     }else{
-        visited = ["start"].into_iter().map(|s|String::from(s)).collect();
+        visited = [String::from("start")].into_iter().collect();
     }
-    for way in ways.clone(){
-        if &way.0 == current && !visited.contains(&way.1){
+    for way in ways.get(current).unwrap(){
+        if !visited.contains(way){
             let mut continued_path = path.clone();
-            if way.1 == "end"{
+            if *way == "end"{
                 *finished_paths +=1;
             }else{
-                if actual_visited.contains(&way.1){
+                if actual_visited.contains(way){
                     continued_path.1 = true;
                 }
-                continued_path.0.push(way.1);
-                new_paths.push(continued_path);
-            }
-        }
-        else if &way.1 == current && !visited.contains(&way.0){
-            let mut continued_path = path.clone();
-            if way.0 == "end"{
-                *finished_paths +=1;
-            }else{
-                if actual_visited.contains(&way.0){
-                    continued_path.1 = true;
+                continued_path.0.push(String::from(way));
+                let mut new_visited = actual_visited.clone(); 
+                if small_cave(way){
+                    new_visited.insert(String::from(way));
                 }
-                continued_path.0.push(way.0);
-                new_paths.push(continued_path);
+                find_continued(ways.clone(), continued_path, finished_paths,new_visited);
             }
         }
     }
